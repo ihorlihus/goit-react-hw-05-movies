0700895b043c2165e67cc445ca281791d733fe78
ihorlihus <ligusigor@ukr.net>
2022-09-11 11:50:57 +0300

    fetch

--- src/API/GetMoves.jsx
+++ src/API/GetMoves.jsx
@@ -1,21 +1,23 @@
 const axios = require('axios').default;
 // https://api.themoviedb.org/3/movie/550?api_key=f632edb05cc91b97d9ccb27096819906
 
-axios.defaults.baseURL = 'https://api.themoviedb.org/3/movie/550';
+axios.defaults.baseURL = 'https://api.themoviedb.org/3/search/movie';
 const myKey = 'f632edb05cc91b97d9ccb27096819906';
 
 async function getMoves(searchName) {
   const responce = await axios.get('', {
     params: {
       api_key: myKey,
+      language: 'en-US',
       query: searchName,
       include_adult: true,
       safesearch: true,
       page: 1,
     },
   });
-  console.log(responce);
-  return responce.data;
+  const moves = await responce.data.results;
+  return moves;
 }
 
+// getMoves('batman').then(res => console.log(res));
 export default getMoves;
--- src/pages/Moves.jsx
+++ src/pages/Moves.jsx
@@ -1,13 +1,13 @@
+import { useState, useEffect } from 'react';
 import { useSearchParams } from 'react-router-dom';
 // import { ProductList } from "../components/ProductList";
 import { SearchBox } from '../components/SearchBox';
 import getMoves from './../API/GetMoves';
 
 export const Moves = () => {
-  const moves = getMoves();
+  const [movesState, setMovesState] = useState([]);
   const [searchParams, setSearchParams] = useSearchParams();
   const moveName = searchParams.get('name') ?? '';
-  console.log(moves);
 
   //   const visibleMoves = moves.filter(move =>
   //     move.name.toLowerCase().includes(moveName.toLowerCase())
@@ -18,9 +18,30 @@ export const Moves = () => {
     setSearchParams(nextParams);
   };
 
+  useEffect(() => {
+    // Тут выполняем асинхронную операцию,
+    // например HTTP-запрос за информацией о пользователе
+    if (moveName === '') return;
+
+    async function fetchMoves() {
+      const moves = await getMoves(moveName);
+      console.log(moves);
+      setMovesState(moves);
+    }
+
+    fetchMoves();
+  }, [moveName]);
+
   return (
     <main>
       <SearchBox value={moveName} onChange={updateQueryString} />
+      <ul>
+        {movesState.map(move => (
+          <li key={move.id}>
+            <img src={`${move.poster_path}`} alt={move.title} width="240" />
+          </li>
+        ))}
+      </ul>
       {/* <ProductList products={visibleMoves} /> */}
     </main>
   );
